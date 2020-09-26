86bcdaee0e8c9e350e50f5a53032f5af1d9c8735
Yulia Gorshkova <yuuri.sumiren@gmail.com>
2020-09-26 23:28:25 +0300

    Loading list of resources

--- src/Project.hs
+++ src/Project.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Project where
 
+import Control.Applicative ((<|>))
 import Control.Monad
 import qualified Data.Map as M
 import qualified Data.Text.IO as T (readFile)
@@ -9,6 +12,7 @@ import System.IO
 
 import AST
 import Parser
+import Types (Resource (..))
 
 data KeyState = Press | Hold | Release
     deriving (Eq, Ord, Show)
@@ -61,19 +65,46 @@ data Object = Object
     deriving Show
 
 data Project = Project
-    { pScripts :: M.Map String Result
-    , pObjects :: M.Map String Object
+    { pResources :: M.Map String Resource
+    , pScripts   :: M.Map String Result
+    , pObjects   :: M.Map String Object
     }
     deriving Show
 
+resDir :: Resource -> FilePath
+resDir = \case
+    RSprite     -> "sprites"
+    RSound      -> "sounds"
+    RBackground -> "backgrounds"
+    RRoom       -> "rooms"
+
+logTrace :: String -> IO ()
+logTrace = hPutStrLn stderr
+
+loadResources :: FilePath -> Resource -> IO (M.Map String Resource)
+loadResources path ty = do
+    let rPath = path </> resDir ty
+    rNames <- listDirectory rPath
+    logTrace $ "Loading resources from " ++ rPath
+    return $ M.fromList [(res, ty) | res <- rNames]
+    <|>
+    return M.empty
+
 loadProject :: FilePath -> IO Project
 loadProject path = do
+    -- Load resources
+    resources <- mapM (loadResources path)
+        [ RSprite
+        , RSound
+        , RBackground
+        , RRoom
+        ]
     -- Load scripts
     let sDir = path </> "scripts"
     sNames <- listDirectory sDir
     scripts <- forM sNames $ \name -> do
         let sPath = sDir </> name </> name <.> "gml"
-        hPutStrLn stderr $ "Loading a script from " ++ sPath
+        logTrace $ "Loading a script from " ++ sPath
         src <- T.readFile sPath
         return (name, parseSource name src)
     -- Load objects
@@ -83,8 +114,8 @@ loadProject path = do
         eNames <- filter ((== ".gml") . takeExtension) <$> (listDirectory $ oDir </> name)
         events <- forM eNames $ \eName -> do
             let ePath = oDir </> name </> eName
-            hPutStrLn stderr $ "Loading an event from " ++ ePath
+            logTrace $ "Loading an event from " ++ ePath
             src <- T.readFile ePath
             return (parseEvent $ dropExtension eName, parseSource eName src)
         return (name, Object (M.fromList events))
-    return $ Project (M.fromList scripts) (M.fromList objects)
+    return $ Project (M.unions resources) (M.fromList scripts) (M.fromList objects)
--- src/Types.hs
+++ src/Types.hs
@@ -1,22 +1,30 @@
 module Types where
 
 {-| Resource type. In GML it's actually just a number, but here we want to differ. -}
-data Resource = RSprite | RSound | RObject | RRoom
+data Resource
+    = RSprite
+    | RBackground
+    | RSound
+    | RObject
+    | RRoom
+    -- TODO: paths, etc.
     deriving (Eq, Show)
 
 {-| Value type. -}
 data Type
     = TVoid -- ^ Should be used only as a return type
-    | TReal | TString | TArray Type | TArray2 Type
+    | TReal | TString
+    | TArray Type | TArray2 Type
     | TId Resource -- ^ Resource descriptor
     | TUnknown [Type] -- ^ Unknown type with possibilities, if any
     deriving (Eq, Show)
 
-tBool, tInstance, tSprite, tObject :: Type
+tBool, tInstance, tSprite, tObject, tRoom, tUnknown :: Type
 tBool = TReal
 tInstance = TReal
 tSprite = TId RSprite
 tObject = TId RObject
+tRoom = TId RRoom
 tUnknown = TUnknown []
 
 {-| Function or script signature. -}
