baa285264eef50e5ceb979638dc1e38e6180351e
Yulia Gorshkova <yuuri.sumiren@gmail.com>
2020-11-05 12:12:14 +0300

    A few more events

--- src/Language/GML/Events.hs
+++ src/Language/GML/Events.hs
@@ -7,18 +7,22 @@ Events in Game Maker objects.
 
 module Language.GML.Events where
 
-import Data.Char (isAlpha, isDigit)
+import Data.Char (isAlpha, isHexDigit)
 import Text.Read (Read (..))
 import Text.ParserCombinators.ReadP
 import Text.ParserCombinators.ReadPrec (lift)
 
-{-| Key state of keyboard events. -}
+{-| Key state of keyboard or mouse events. -}
 data KeyState = Press | Hold | Release
+    deriving (Eq, Ord, Show)
+
+{-| Button code of mouse events. -}
+data MouseButton = MLeft | MMiddle | MRight
     deriving (Eq, Ord, Show)
 
 {-| Keycode of keyboard events. -}
 data KeyCode = KUp | KDown | KLeft | KRight | KChar Char
-    deriving (Eq, Ord, Show)   
+    deriving (Eq, Ord, Show)
 
 instance Enum KeyCode where
     toEnum x = case x of
@@ -28,11 +32,18 @@ instance Enum KeyCode where
         KLeft -> 37
         KRight -> 39
 
+type Guid = String
+
 {-| Object event. -}
 data Event
     = Create
+    | Destroy
     | Step
+    | Alarm Int
     | Draw
+    | Collision Guid
+    | User Int -- ^ Custom user event
+    | Mouse KeyState MouseButton
     | Key KeyState KeyCode
     deriving (Eq, Ord, Show)
 
@@ -42,10 +53,17 @@ instance Read Event where
 pEvent = do
     event <- munch1 isAlpha
     char '_'
-    code <- toEnum . read <$> munch1 isDigit
+    arg <- munch1 (\c -> isHexDigit c || c == '-')
+    let code = read arg
+    let keycode = toEnum code
     return $ case event of
-        "Create" -> Create
-        "Step"   -> Step
-        "KeyPress"   -> Key Press code
-        "Keyboard"   -> Key Hold code
-        "KeyRelease" -> Key Release code
+        "Create"     -> Create
+        "Destroy"    -> Destroy
+        "Step"       -> Step
+        "Alarm"      -> Alarm code
+        "Collision"  -> Collision arg
+        "Draw"       -> Draw
+        "Other"      -> User (code - 10) --FIXME
+        "KeyPress"   -> Key Press   keycode
+        "Keyboard"   -> Key Hold    keycode
+        "KeyRelease" -> Key Release keycode
