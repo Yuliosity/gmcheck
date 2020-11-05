0458fa95a84ec83576350afc1f8776fc50db1b85
Yulia Gorshkova <yuuri.sumiren@gmail.com>
2020-11-05 03:16:28 +0300

    A stub for the main executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
+import Options.Applicative
+
+import Language.GML.Project
+
+data Options = Options
+    { output :: FilePath
+    , target :: FilePath
+    }
+
+optParser :: Parser Options
+optParser = Options
+    <$> strOption
+        (  long "output"
+        <> short 'o'
+        <> help "Output report" )
+    <*> argument str
+        (  help "Path to the GMS project"
+        <> metavar "TARGET" )
+
+options :: ParserInfo Options
+options = info (helper <*> optParser)
+      ( fullDesc
+     <> progDesc "Run the checker"
+     <> header "gmcheck - a static analysis tool for Game Maker Studio 2 projects" )
+
 main :: IO ()
-main = undefined
+main = do
+    Options { output, target } <- execParser options
+    project <- loadProject target
+    writeFile output $ show project
